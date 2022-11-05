1e440e7ac0c4b593718b355419f5d668e8beabdc
Derrek <a0926884115@gmail.com>
2022-11-05 10:32:45 +0800

    item repairer will fix colonist buildings

--- Source/InvincibleFortress/ItemRepairer.cs
+++ Source/InvincibleFortress/ItemRepairer.cs
@@ -14,6 +14,7 @@ namespace InvincibleFortress
     {
         public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
         {
+
             if (map.listerThings.AllThings.Any((Thing x) => x.def == checkingDef || x.def == checkingDef.blueprintDef || x.def == checkingDef.frameDef || x.def == checkingDef.installBlueprintDef))
             {
                 return new AcceptanceReport("FortressItemRepairerPlaceOnlyOneInMap".Translate());
@@ -38,6 +39,16 @@ namespace InvincibleFortress
                     break;
                 }
             }
+
+            foreach (Building building in map.listerBuildings.allBuildingsColonist)
+            {
+                if (building.def.useHitPoints && building.HitPoints < building.MaxHitPoints)
+                {
+                    building.HitPoints = building.MaxHitPoints;
+                    Log.Message("Building [" + building.def.defName + "] is repaired");
+                }
+            }
+
         }
 
         private bool TryRepairPawnHolder(Pawn pawn)
